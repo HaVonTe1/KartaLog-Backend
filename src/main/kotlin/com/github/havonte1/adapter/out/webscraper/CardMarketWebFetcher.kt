c575802b3744daa84021dfba73a9fdc7298b031d
Dirk Kutzer <dirk.kutzer@bdr.de>
2026-02-06 22:43:25 +0100

    add geolocation and storage state handling in CardMarketWebFetcher

--- src/main/kotlin/com/github/havonte1/adapter/out/webscraper/CardMarketWebFetcher.kt
+++ src/main/kotlin/com/github/havonte1/adapter/out/webscraper/CardMarketWebFetcher.kt
@@ -1,12 +1,16 @@
 package com.github.havonte1.adapter.out.webscraper
 
 import com.microsoft.playwright.Browser
+import com.microsoft.playwright.BrowserContext
 import com.microsoft.playwright.BrowserType.LaunchOptions
 import com.microsoft.playwright.options.LoadState
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
+import java.io.File
+import java.nio.file.Paths
+import kotlin.io.path.exists
 
 /**
  * Responsible for performing the HTTP fetch using Playwright.
@@ -31,9 +35,16 @@ class CardMarketWebFetcher {
 
             )
             val contextOptions = Browser.NewContextOptions()
-
+                .setGeolocation(52.5200, 13.4050) // Berlin: Lat, Long
+                .setPermissions(listOf("geolocation")) // Berechtigung automatisch erteilen
                 .setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36")
 
+            val storageFile = Paths.get("auth.json")
+
+            if(storageFile.exists()) {
+                contextOptions.setStorageStatePath(storageFile)
+
+            }
             val context = browser.newContext(contextOptions)
             val page: Page = context.newPage()
             val url = "https://www.cardmarket.com/de/Pokemon/Products/Search?searchString=$searchString"
@@ -44,6 +55,11 @@ class CardMarketWebFetcher {
 
             val content = page.content()
             logger.debug { "Fetched content length: ${content.length}" }
+
+            // Zustand (Cookies & LocalStorage) in Datei speichern
+            context.storageState(
+                BrowserContext.StorageStateOptions()
+                .setPath(Paths.get("auth.json")))
             browser.close()
             return content
         }
