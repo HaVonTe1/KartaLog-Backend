dd178e2a6e22a2e2b910a419704c5a88565df418
Dirk Kutzer <dirk.kutzer@bdr.de>
2026-02-14 09:06:47 +0100

    minor fixes

---
+++ src/main/kotlin/io/github/havonte1/tcgwatcher/backend/config/CardMarketConstants.kt
@@ -0,0 +1,7 @@
+package io.github.havonte1.tcgwatcher.backend.config
+
+object CardMarketConstants {
+    const val DEFAULT_LOCALE: String = "de"
+    const val DEFAULT_GAME: String = "Pokemon"
+    const val PATH_SEPARATOR: String = "/"
+}
